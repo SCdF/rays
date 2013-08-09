b937b9cd0bb5b381d2324d9802e33d96b2476eda
SCdF <stefan@sdufresne.info>
2013-08-09 20:40:20 +1200

    Added type hints

--- src/rays/core.clj
+++ src/rays/core.clj
@@ -4,25 +4,23 @@
            java.io.File
            javax.imageio.ImageIO))
 
-(defn buffered-image ^BufferedImage
+(set! *warn-on-reflection* true)
+
+(defn buffered-image
   [data]
   (let [height (count data)
         width (count (first data))
-        bimage ^BufferedImage (BufferedImage. width height BufferedImage/TYPE_INT_RGB)]
+        bimage (BufferedImage. width height BufferedImage/TYPE_INT_RGB)]
     (doseq [x (range width)
             y (range height)]
       (let [^Color cval ((data y) x)]
         (.setRGB bimage x y (.getRGB cval))))
     bimage))
 
-(defn image->file
-  [image filename]
-  (ImageIO/write image "png" (File. filename)))
+(defn image->file [^BufferedImage image ^String filename] (ImageIO/write image "png" (File. filename)))
 
-
-(defn- silly-random-color ^Color
-  [_]
-  (Color. (rand-int 255) (rand-int 255) (rand-int 255)))
+(defn- silly-random-color [_]
+  (Color. ^int (rand-int 255) ^int (rand-int 255) ^int (rand-int 255)))
 
 (defn- silly-random-image-row [width]
   (vec (map silly-random-color (range width))))
